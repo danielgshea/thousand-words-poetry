f5231b120bf0e3dba0e36d5cd3cda8e832595e9a
dgshea <dgshea@ncsu.edu>
2024-03-25 16:11:50 -0400

    learned how to make a grid using flexbox smh

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,19 +2,32 @@ import React from "react";
 import theme from "../theme/theme";
 
 const Home: React.FC = () => {
+
+
   return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        background: theme.palette.background.main,
-      }}
-    >
-      <h1 style={{ margin: "0" }}>Home page</h1>
-    </div>
+      <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
+        <div style={{ flex: 1 }}>
+          <div style={{ height: "100%", backgroundColor: "red" }}>
+            row 1 col 1
+            <div style={{ height: "100%", backgroundColor: "blue" }}></div>
+          </div>
+        </div>
+        <div style={{ display: "flex", flex: 1 }}>
+          {/* Row 2 */}
+          <div style={{ flex: 1, backgroundColor: "green" }}>
+            row 2 col 1
+            <div style={{ height: "100%", backgroundColor: "yellow" }}></div>
+          </div>
+          <div style={{ flex: 4, backgroundColor: "orange" }}>
+            row 2 col 2
+            <div style={{ height: "100%", backgroundColor: "purple" }}></div>
+          </div>
+        </div>
+        <div style={{ flex: 1 }}>
+          {/* Row 3 */}
+          <div style={{ height: "100%", backgroundColor: "pink" }}>row 3</div>
+        </div>
+      </div>
   );
 };
 
